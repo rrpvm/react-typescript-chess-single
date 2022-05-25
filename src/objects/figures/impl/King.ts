229d8eba30369e5963cf3d79a2b54abb5852049b
rrpvm <funikovsasa@gmail.com>
2022-05-25 18:26:15 +0700

    add king logic

--- src/objects/figures/impl/King.ts
+++ src/objects/figures/impl/King.ts
@@ -12,7 +12,18 @@ export default class King extends Figure {
         return this._player === Players.PLAYER_BLACK ? 'https://www.symbols.com/images/symbol/1/3398_black-king.png' : 'https://www.symbols.com/images/symbol/1/3404_white-king.png';
     }
     public override  canRelocate(src: BoardCell): Pair<number, number>[] {
-        return [];
+        let result: Pair<number, number>[] = [];
+        for (let i = -2; i < 2; i++) {
+            for (let j = -2; j < 2; j++) {
+                const _lx = Math.max(Math.min(src.xy.left - i, 7), 0);
+                const _ly = Math.max(Math.min(src.xy.right - j, 7), 0);
+                if (Math.sqrt(Math.pow(src.xy.left - _lx, 2) + Math.pow(src.xy.right - _ly, 2)) <= Math.sqrt(2)) {
+                    if (!this.isFriendly(this._get_cell_function(_lx, _ly), src))
+                        result.push(new Pair(_lx, _ly));
+                }
+            }
+        }
+        return result;
     }
 
 }
